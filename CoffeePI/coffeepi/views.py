5174abf2237b6aa5c9535830770a7304ac6efcb8
Joe Reid <jcreid.computing@gmail.com>
2013-06-15 15:15:47 +0100

    Hello World
    Added hello world route to the pyramid setup
    Created hello view

--- CoffeePI/coffeepi/views.py
+++ CoffeePI/coffeepi/views.py
@@ -4,3 +4,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'CoffeePI'}
+
+@view_config(route_name='hello')
+def hello(request):
+    request.response.text = 'hello world'
+    return request.response
